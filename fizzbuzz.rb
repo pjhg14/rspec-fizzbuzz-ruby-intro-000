5f474f29727ca804713db5d276e3ce906a505db5
Paul H Graham Jr <pjhg14@gmail.com>
2021-01-30 13:27:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,8 @@ def fizzbuzz(number)
   if number % 5 == 0 && number % 3 == 0
     "FizzBuzz"
   elsif number % 3 == 0
-    "Buzz"
-  elsif  number % 5 == 0
     "Fizz"
+  elsif  number % 5 == 0
+    "Buzz"
   end
 end
